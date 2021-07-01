e3af3af67561f8d79dfb0f183b0b02918eeb3fc3
frleal2 <45012732+frleal2@users.noreply.github.com>
2021-06-30 23:51:31 -0600

    fixed a segmentation error

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,87 +1,80 @@
 #include "tokenizer.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int space_char(char c){
-  switch(c){
-      case '\n':
-      case '\t':
-      case ' ':
-	return 1;
-     default:
-	  return 0;
-    }
+char *copy_str(char *str, short length){
+  char *tokens = (char *)malloc(sizeof(char) * (length + 1));
+  
+  int i = 0;
+  while(i < length && str[i] != '\0'){
+    tokens[i] = str[i];
+    i++;
+  }
+  tokens[length] = '\0';
+  return tokens;
 }
 
+/*Evaluates weather a character is valid*/
+int space_char(char c){
+  switch(c){
+    case '\n':
+    case '\t':
+    case ' ':
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+/*Evaluates weather a character is invalid*/
 int non_space_char(char c){
-  if(c != '\t' && c != '\n' && c != ' '){
-    return 1;
+  if(c != '\t' && c != ' ' && c != '\n'){
+     return 1;
   }
   else{
     return 0;
-  }
+  }  
 }
 
-char word_start(char *str){
-  while(space_char(*str) == 1 ){
+/*Function to find the start of a word*/
+
+char *word_start(char *str){
+  while(space_char(*str) == 1){
+    str++;
+  }  
+  return str;
+}
+
+/*Function to find the end of a word*/
+char *word_terminator(char *str){
+  while(non_space_char(*str) == 1 && *str != '\0'){
     str++;
   }
   return str;
 }
 
-char *word_terminator(char *word){
-  while(non_space_char(*word) == 1 && *word != '\0'){
-    word++;
-  }
-  return word;
-}
-
+/*Function to find the number of words there is in a sentence or sentence*/
 int count_words(char *str){
-  int counter= 0;
+  int counter = 0;
   while(*str != '\0'){
     str = word_start(str);
-    str = word_terminator(str);
+    str = word_end(str);
     counter++;
   }
   return counter;
+  
 }
 
-char *copy_str(char *inStr, short len){
-  char *tokens = (char *)malloc(sizeof(char) * (len + 1));
-  int i = 0;
-  while(i < len && inStr[i] != '\0'){
-    tokens[i] = inStr[i];
-    i++;
-  }
-  tokens[len] = '\0';
-  return tokens;
-}
-
-char **tokenize(char *str){
-  short words_num = count_words(str);
-  short word_length = 0;
-
-  char** tokens = (char **)malloc((words_num + 1)*sizeof(char*));
-
-  for(short i = 0; i< words_num; i++){
-    char *start = word_start(str); // points to the beginning of a word
-    char *end = word_terminator(str); //points to the end of same word
-    int word_length = (int)(end-start) + 1; //length of each word
-    tokens[i] = copy_str(start, word_length); // allocates all memory and copies all words
-    str = word_start(end); //makes the sentence start at the beginning of the next word.
-  }
-  tokens[words_num] = '\0';
-  return tokens;
-}
-
-void print_tokens(char **tokens){
+/* Function that prints all tokens*/
+void print_tokens(char** tokens){
   while(*tokens){
-    printf("[%s] ", *tokens);
+    printf("[%s]   ", *tokens);
     tokens++;
   }
 }
 
-void free_tokens(char **tokens){
+/*Function that frees all tokens and the array containing the tokens. */
+void free_tokens(char** tokens){
   char** temp = tokens;
   while(*tokens){
     free(*tokens);
@@ -90,6 +83,27 @@ void free_tokens(char **tokens){
   free(temp);
 }
 
-
-				
-
+/*Function that tokenizes the string that is passed on to the argument into an array of tokens*/
+char** tokenize(char* sentence){
+  short words_num = count_words(sentence);
+  short word_leng =0;
+  char** tokens = (char**) malloc((words_num+1)*sizeof(char*));//we allocate memory for the number of pointers needed
+  
+  for(short i=0;i<words_num;i++){ //we will calculate length of each string and allocate memory for each
+    
+    char *start = word_start(sentence); //pointer at the beginning of a word.
+    
+    char *end = word_end(sentence);  //pointer at the end of a word.
+    
+    int word_leng = (int)(end - start) + 1; //This computes the lenght of each word.
+    
+    tokens[i] = copy_str(start, word_leng); //allocates the memory for all the words and copies each word into a token.
+    
+    sentence = word_start(end);  //Makes the sentence start at the beggining of the next word.
+    
+    //tokens[i][word_leng] = '\0';
+    
+  }
+  tokens[words_num] = '\0';
+  return tokens;
+}
